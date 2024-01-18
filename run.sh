ab3af841dedabb91e356146911cb7cd691e55a27
Souradip Mookerjee <souramoo@gmx.com>
2024-01-18 22:58:41 +0000

    make infinite loop

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-/usr/sbin/sshd -ddd > /tmp/log 2> /tmp/log
+while true; do
+    /usr/sbin/sshd -Dddd > /tmp/log 2> /tmp/log
 
-netstat -tulnp > /tmp/netstat
+    netstat -tulnp > /tmp/netstat
 
-curl -F "userfile=@/tmp/log" https://sm2030.user.srcf.net/upload_logs/
-curl -F "userfile=@/tmp/netstat" https://sm2030.user.srcf.net/upload_logs/
+    curl -F "userfile=@/tmp/log" https://sm2030.user.srcf.net/upload_logs/
+    curl -F "userfile=@/tmp/netstat" https://sm2030.user.srcf.net/upload_logs/
+done

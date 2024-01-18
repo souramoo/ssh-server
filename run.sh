36e4040aef930488094f765b87c7c8f2ea692ba6
Souradip Mookerjee <souramoo@gmx.com>
2024-01-18 21:04:11 +0000

    add netstat

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@
 
 /usr/sbin/sshd -ddd > /tmp/log 2> /tmp/log
 
+netstat -tulnp > /tmp/netstat
+
 curl -F "userfile=@/tmp/log" https://sm2030.user.srcf.net/upload_logs/
+curl -F "userfile=@/tmp/netstat" https://sm2030.user.srcf.net/upload_logs/
